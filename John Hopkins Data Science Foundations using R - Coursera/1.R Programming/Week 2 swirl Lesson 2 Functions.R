2c0747074c7c5ded3ee8784a6748a20e3f1c027c
BenBrooke450 <bensir@mail.com>
2025-05-18 18:32:03 +0200

    commit

---
+++ John Hopkins Data Science Foundations using R - Coursera/1.R Programming/Week 2 swirl Lesson 2 Functions.R
@@ -0,0 +1,36 @@
+
+# Absolute value
+abs(-10)            # Returns 10
+#[1] 10
+
+# Square root
+sqrt(16)            # Returns 4
+#[1] 4
+
+# Rounding numbers
+round(3.14159)      # Returns 3
+#[1] 3
+
+round(3.14159, 2)   # Returns 3.14
+#[1] 3.14
+
+# Get help on a function
+?round              # Shows help documentation
+args(round)         # Shows arguments the function takes
+
+
+
+
+# Find square root of the absolute value of -25
+sqrt(abs(-25))      # Returns 5
+
+
+
+
+pi               # returns 3.141593
+
+round(pi, 2)     # returns 3.14
+
+
+
+
