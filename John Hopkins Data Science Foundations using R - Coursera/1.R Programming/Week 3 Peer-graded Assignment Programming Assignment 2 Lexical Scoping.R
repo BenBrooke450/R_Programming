ef20570fc743a867327aa631a65c66ca78261453
BenBrooke450 <bensir@mail.com>
2025-05-19 22:16:42 +0200

    commit

--- John Hopkins Data Science Foundations using R - Coursera/1.R Programming/Week 3 Peer-graded Assignment Programming Assignment 2 Lexical Scoping.R
+++ John Hopkins Data Science Foundations using R - Coursera/1.R Programming/Week 3 Peer-graded Assignment Programming Assignment 2 Lexical Scoping.R
@@ -17,25 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setinverse(inv)
-  inv
-}
 
-
-m <- matrix(c(1, 2, 3, 4), 2, 2)
-cm <- makeCacheMatrix(m)
-cacheSolve(cm)
-     [,1] [,2]
-[1,]   -2  1.5
-[2,]    1 -0.5
 
 
 cacheSolve(cm)
