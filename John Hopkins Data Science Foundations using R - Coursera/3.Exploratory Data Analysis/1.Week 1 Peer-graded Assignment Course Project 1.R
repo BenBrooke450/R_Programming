90ac38d69f0eb1a3fdad5629203321d04b8f8ce7
BenBrooke450 <bensir@mail.com>
2025-05-21 17:53:10 +0200

    commit

--- John Hopkins Data Science Foundations using R - Coursera/3.Exploratory Data Analysis/1.Week 1 Peer-graded Assignment Course Project 1.R
+++ John Hopkins Data Science Foundations using R - Coursera/3.Exploratory Data Analysis/1.Week 1 Peer-graded Assignment Course Project 1.R
@@ -28,6 +28,43 @@ plot(subset_data$DateTime, subset_data$Global_active_power,
 dev.off()
 
 
+png("plot3.png", width = 480, height = 480)
+plot(subset_data$DateTime, subset_data$Sub_metering_1, type = "l",
+     xlab = "", ylab = "Energy sub metering")
+lines(subset_data$DateTime, subset_data$Sub_metering_2, col = "red")
+lines(subset_data$DateTime, subset_data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), lty = 1,
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
+
+
+
+
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2, 2))
+
+# Top-left
+plot(subset_data$DateTime, subset_data$Global_active_power,
+     type = "l", xlab = "", ylab = "Global Active Power")
+
+# Top-right
+plot(subset_data$DateTime, subset_data$Voltage,
+     type = "l", xlab = "datetime", ylab = "Voltage")
+
+# Bottom-left
+plot(subset_data$DateTime, subset_data$Sub_metering_1,
+     type = "l", xlab = "", ylab = "Energy sub metering")
+lines(subset_data$DateTime, subset_data$Sub_metering_2, col = "red")
+lines(subset_data$DateTime, subset_data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), lty = 1,
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       bty = "n")
+
+# Bottom-right
+plot(subset_data$DateTime, subset_data$Global_reactive_power,
+     type = "l", xlab = "datetime", ylab = "Global_reactive_power")
+
+dev.off()
 
 
 
