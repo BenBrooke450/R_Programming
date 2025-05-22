74609d70b75f186e1af6e822833cc300412cb92a
BenBrooke450 <bensir@mail.com>
2025-05-22 17:58:12 +0200

    commit

--- John Hopkins Data Science Foundations using R - Coursera/3.Exploratory Data Analysis/2.Week 4 Peer-graded Assignment Course Project 2.R
+++ John Hopkins Data Science Foundations using R - Coursera/3.Exploratory Data Analysis/2.Week 4 Peer-graded Assignment Course Project 2.R
@@ -1,9 +1,30 @@
 
+# Set the CRAN mirror to use (this one is in the UK, but you can use any)
+options(repos = c(CRAN = "https://cloud.r-project.org"))
 
-library(ggplot2)
+# Now install the package
+install.packages("dplyr")
+install.packages("ggplot2")
+
 library(dplyr)
+library(ggplot2)
+
+
+NEI <- readRDS("/Users/benjaminbrooke/Desktop/Data/exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("/Users/benjaminbrooke/Desktop/Data/exdata_data_NEI_data/Source_Classification_Code.rds")
+
+
+total_emissions <- NEI %>%
+  group_by(year) %>%
+  summarise(Total = sum(Emissions, na.rm = TRUE))
 
 
 
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+
+
+#Question 1: Total PM2.5 Emissions (US-wide)
+png("plot1.png", width = 480, height = 480)
+barplot(total_emissions$Total / 1e6, names.arg = total_emissions$year,
+        col = "skyblue", main = "Total PM2.5 Emissions in the US",
+        xlab = "Year", ylab = "Emissions (millions of tons)")
+dev.off()
