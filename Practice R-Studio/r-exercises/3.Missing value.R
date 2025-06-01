073be846db66087dc36da78a91bc73c40c62191e
BenBrooke450 <bensir@mail.com>
2025-06-01 16:47:41 +0000

    Basic

--- Practice R-Studio/r-exercises/3.Missing value.R
+++ Practice R-Studio/r-exercises/3.Missing value.R
@@ -9,6 +9,7 @@ length(X)
 
 
 
+
 #Exercise 2
 #If X = c(NA,3,14,NA,33,17,NA,41) write some R code that will remove all occurrences of NA in X.
 #a. X[!is.na(X)]
@@ -19,6 +20,7 @@ Y = c(NA,3,14,NA,33,17,NA,41)
 
 Y[!is.na(Y)]
 #3 14 33 17 41
+
 
 
 
@@ -36,6 +38,7 @@ Q[is.na(Q)]= 11
 
 print(Q)
 #[1]  1  3 12 11 33  7 11 21
+
 
 
 
@@ -81,6 +84,7 @@ print(W)
 
 
 
+
 #Exercise 7
 #Consider the following vector A <- c (33, 21, 12, NA, 7, 8) .
 #Write some R code that will calculate the mean of A without the missing value.
@@ -102,6 +106,34 @@ print(A)
 
 
 
+#Exercise 8
+#Let:
+c1 <- c(1,2,3,NA)
+c2 <- c(2,4,6,89)
+c3 <- c(45,NA,66,101)
+X <- rbind (c1,c2,c3, deparse.level=1) # write a code that will display all rows with missing values.
+
+print(X)
+#    [,1] [,2] [,3] [,4]
+#c1    1    2    3   NA
+#c2    2    4    6   89
+#c3   45   NA   66  101
+
+
+clean_data = X[complete.cases(X),]
+clean_data
+#[1]  2  4  6 89 ONLY ONE ROW LEFT
+
+
+clean_data_two <- na.omit(X)
+print(clean_data_two)
+#    [,1] [,2] [,3] [,4]
+#c2    2    4    6   89
+
+
+clean_data_third = X[!is.na(X)]
+clean_data_third
+#[1]   1   2  45   2   4   3   6  66  89 101 INCORRECT
 
 
 
@@ -112,8 +144,56 @@ print(A)
 
 
 
+#Exercise 9
+#Consider the following data obtained from 
+df <- data.frame(Name = c(NA, "Joseph", "Martin", NA, "Andrea"), Sales = c(15, 18, 21, 56, 60),
+                  Price = c(34, 52, 21, 44, 20), stringsAsFactors = FALSE)
+#Write some R code that will return a data frame which removes all rows with NA values in Name column
+
+print(df)
+#    Name Sales Price
+#1   <NA>    15    34
+#2 Joseph    18    52
+#3 Martin    21    21
+#4   <NA>    56    44
+#5 Andrea    60    20
+
+
+clean_data_four <- df[!is.na(df$Name), ]
+clean_data_four
+#   Name   Sales  Price
+#2 Joseph    18    52
+#3 Martin    21    21
+#5 Andrea    60    20
 
 
 
+
+
+
+
+#Exercise 10
+#Consider the following data obtained from 
+df <- data.frame(Name = c(NA, "Joseph", "Martin", NA, "Andrea"), 
+                 Sales = c(15, 18, 21, NA, 60), Price = c(34, 52, 33, 44, NA), stringsAsFactors = FALSE)
+#Write some R code that will remove all rows with NA values and give the following output
+
+
+clean_data_five <- df[!is.na(df$Name), ]
+clean_data_five
+#  Name    Sales Price
+#2 Joseph    18    52
+#3 Martin    21    33
+#5 Andrea    60    NA
+
+
+
+clean_data_six <- df[!is.na(df$Price), ]
+clean_data_six
+#   Name Sales Price
+#1   <NA>    15    34
+#2 Joseph    18    52
+#3 Martin    21    33
+#4   <NA>    NA    44
 
 
